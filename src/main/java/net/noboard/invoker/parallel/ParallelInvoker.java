79bbac263ea6a87006fcafcc5c31e0fbb2a1a33b
wanxiangming1994 <miniqueue@foxmail.com>
2019-03-26 15:52:47 +0800

    wanxm

--- src/main/java/net/noboard/invoker/parallel/ParallelInvoker.java
+++ src/main/java/net/noboard/invoker/parallel/ParallelInvoker.java
@@ -11,35 +11,6 @@ import java.util.function.Consumer;
 
 /**
  * 并行执行器
- *
- * reject保证的是：
- * 当reject的时候，主线程停止等待；当reject的时候，还未启动的串行任务（线程）将不会被启动；
- * 当reject的时候，catched立即执行；只有第一个到达的reject会执行catched，后续将被拒绝（跳过）；
- * 当reject的时候，主线程会等待直到catched回调执行完毕
- *
- * reject不能的是：
- * 打断当前线程；
- * 打断并行线程；
- * 打断并行线程的创建（reject不能打断创建一组并行任务，即，一旦开始创建一组并行任务，它们将被成功创建）；
- * ## 综上，reject不能打断线程，如果你想让线程提前结束，需要自己使用return语句，但你也仅能打断当前线程 ##
- *
- * continued用来标记一组并行任务，该组任务的执行不会阻塞主线程，但该组任务有能力触发reject。
- * 如果reject触发时主程序已经进入“正常结束预备状态”或“结束状态”，则catched不会被调用。
- * 注意：如果执行器再次开始新一轮执行，而此时上一次执行中的continued任务组触发了reject，则catched
- * 会被执行。
- *
- * 如果执行器处于“执行中状态”，start方法不会再次启动执行器
- *
- * ## 说明
- *
- * java提供了AtomicBoolean类，我考虑过使用这个类，但当intoRunningStatus方法被并发
- * 的调用时，可能出现A线程通过了if判断，B线程也通过了if判断，此时会导致A,B两个线程
- * 都能使执行器启动，导致线程不安全。所以（其他“into”方法同理）需要使用synchronized
- * 关键字，将进入某种状态时的判断和写入操作改写为原子性的。call方法（及和它类似的then，
- * and等接口）也不能使用AtomicBoolean，当A线程通过了call方法中的if语句，B线程获得了CPU
- * 执行，从而开始运行invoke函数，此时A线程再次获得CPU执行，从而修改了invoker chain，这
- * 就会导致线程不安全问题。综上，使用synchronized关键字是比较合理的做法。
- *
  * @author wanxm
  */
 public class ParallelInvoker implements Invoker {
@@ -159,7 +130,9 @@ public class ParallelInvoker implements Invoker {
     @Override
     public void reject(Exception e) {
         if (this.intoCatchStatus()) {
-            this.onReject(e);
+            while (currentChainThreadLock.getCount() > 0) {
+                currentChainThreadLock.countDown();
+            }
             if (this.catchConsumer != null) {
                 this.catchConsumer.accept(e);
                 mainThreadLock.countDown();
@@ -170,7 +143,7 @@ public class ParallelInvoker implements Invoker {
     /**
      * 请求进入捕获状态，返回成功或者失败
      *
-     * 如果不在捕获状态，且在运行中状态，则可以进入捕获状态（返回true），且将状态置为捕获状态；
+     * 如果不在捕获状态，且在运行中状态，且不在正常结束状态，则可以进入捕获状态（返回true），且将状态置为捕获状态；
      * 如果已经在捕获状态，则不可以再次进入捕获状态（返回false）。
      *
      * @return
@@ -257,19 +230,6 @@ public class ParallelInvoker implements Invoker {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
-    }
-
-    /**
-     * 打开主线程锁，让主线程继续执行
-     *
-     * 在调用catched consumer之前调用此方法。因为e会被传递给catched consumer，
-     * 在catched consumer之前调用此方法可以保证它所接收的e和reject时的e是一致的。
-     * @param e
-     */
-    private void onReject(Exception e) {
-        while (currentChainThreadLock.getCount() > 0) {
-            currentChainThreadLock.countDown();
         }
     }
 }
